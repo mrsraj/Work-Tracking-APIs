499112435905a1ddf97808eb4f9d136bcaeec2e8
Sachin Raj <sachinraj6392@gmail.com>
2025-02-06 14:35:56 +0530

    modify get_task and get_group function

--- wtapp/models.py
+++ wtapp/models.py
@@ -10,23 +10,22 @@ class CustomUser (AbstractUser):
     def __str__(self):
         return self.username
     
-class Task(models.Model):
-    title = models.CharField(max_length=255)
-    desc = models.TextField(blank=True, null=True)
-    date = models.DateField(auto_now_add=True)
-    column = models.CharField(max_length=20)  # e.g., column1, column2, column3
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=1)
-
-    def __str__(self):
-        return self.title
-    
-
 class Board(models.Model):
     creation_timestamp = models.DateTimeField(auto_now_add=True)
     created_by = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
+    
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField(blank=True, null=True)
+    date = models.DateField(auto_now_add=True)
+    column = models.CharField(max_length=20)  # e.g., column1, column2, column3
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default='Null')
+    bord_id = models.ForeignKey(Board, on_delete=models.CASCADE, default='Null')
 
+    def __str__(self):
+        return self.title
 
 class BoardMember(models.Model):
     creation_timestamp = models.DateTimeField(auto_now_add=True)
